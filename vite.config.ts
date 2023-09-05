e496cc1b00a1c1b9187a12cab84acea537c55cda
Aleix <nigeon@gmail.com>
2023-09-05 11:46:28 +0200

    removed unnecessary config from vite polyfills config

--- vite.config.ts
+++ vite.config.ts
@@ -24,19 +24,6 @@ export default defineConfig({
     tsconfigPaths(),
     react(),
     nodePolyfills({
-      // To add only specific polyfills, add them here. If no option is passed, adds all polyfills
-      include: ["path"],
-      // To exclude specific polyfills, add them to this list. Note: if include is provided, this has no effect
-      exclude: [
-        "fs", // Excludes the polyfill for `fs` and `node:fs`.
-      ],
-      // Whether to polyfill specific globals.
-      globals: {
-        Buffer: true, // can also be 'build', 'dev', or false
-        global: true,
-        process: true,
-      },
-      // Whether to polyfill `node:` protocol imports.
       protocolImports: true,
     }),
   ],
